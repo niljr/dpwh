ef5fc38a2a22c723790135e6c58acc9ddaa63932
Nil <niljimenezjr@gmail.com>
2021-03-24 07:01:20 +0800

    first commit

--- src/components/base/Navbar/Navbar.js
+++ src/components/base/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import { IoIosNotifications, IoMdArrowDropdown } from 'react-icons/io';
 import Typography from '../Typography/Typography';
 import './navbar.scss';
 
@@ -7,7 +8,7 @@ type Props = {
     className?: string
 }
 
-const appName = process.env.REACT_APP_SITE_TITLE;
+// const appName = process.env.REACT_APP_SITE_TITLE;
 
 // NOTE: this is just a sample nav
 // please update when needed
@@ -15,7 +16,17 @@ const appName = process.env.REACT_APP_SITE_TITLE;
 export default function Navbar({ className = '' }: Props): React$Element<any> {
     return (
         <div className={`navbar ${className}`} >
-            <Typography variant='size-24'>{appName}</Typography>
+            <div className='navbar__left'>
+                <Typography variant='size-12'>Welcome</Typography>
+                <Typography variant='size-18' weight='bold'>Dingdong, Dantes</Typography>
+                <Typography variant='size-12'>Iloilo 3rd district Engineering Office</Typography>
+            </div>
+            <div className='navbar__right'>
+                <IoIosNotifications size={20} className='m-3' />
+                <Typography variant='size-20'>Home</Typography>
+                <IoMdArrowDropdown size={20}/>
+            </div>
+
         </div>
     );
 }
